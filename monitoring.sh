e6727dc342080578e0e49f87cdb989456de94f7b
zhy2on <ojh5940@naver.com>
2021-08-12 17:21:43 +0900

    edit monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,47 +1,33 @@
 #!/bin/bash
+arc=$(uname -a)
+pcpu=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+fdisk=$(df -Bg | grep '^/dev/mapper' | awk '{ft += $2} END {print ft}')
+udisk=$(df -Bm | grep '^/dev/mapper' | awk '{ut += $3} END {print ut}')
+pdisk=$(df -Bm | grep '^/dev/mapper' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lvmt=$(lsblk | grep "lvm" | wc -l)
+lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
+#You need to install net tools for the next step [$ sudo apt install net-tools]
+ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+ulog=$(users | wc -w)
+ip=$(hostname -I)
+mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
+cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l) # journalctl should be running as sudo but our script is running as root so we don't need in sudo here
+wall "	#Architecture: $arc
+	#CPU physical: $pcpu
+	#vCPU: $vcpu
+	#Memory Usage: $uram/${fram}MB ($pram%)
+	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
+	#CPU load: $cpul
+	#Last boot: $lb
+	#LVM use: $lvmu
+	#Connexions TCP: $ctcp ESTABLISHED
+	#User log: $ulog
+	#Network: IP $ip ($mac)
+	#Sudo: $cmds cmd" # broadcast our system information on all terminals
 
-printf "#Architecture: "
-uname -a
-
-printf "#CPU physical : "
-grep "physical id" /proc/cpuinfo | sort -u | wc -l
-
-printf "#vCPU : "
-grep "^processor" /proc/cpuinfo | wc -l
-
-printf "#Memory Usage: "
-free -m | grep Mem | awk '{printf"%d/%dMB (%.2f%%)\n", $3, $2, $3/$2 * 100}'
-
-printf "#Disk Usage: "
-df -BM -a | grep /dev/mapper/ | awk '{sum+=$3}END{print sum}' | tr -d '\n'
-printf "/"
-df -BM -a | grep /dev/mapper/ | awk '{sum+=$2}END{print sum}' | tr -d '\n'
-printf "MB ("
-df -BM -a | grep /dev/mapper/ | awk '{sum1+=$3 ; sum2+=$2 }END{printf "%d", sum1 / sum2 * 100}' | tr -d '\n'
-printf "%%)\n"
-
-printf "#CPU load: "
-mpstat | grep all | awk '{printf "%.2f%%\n", 100-$13}'
-
-printf "#Last boot: "
-who -b | sed 's/^ *system boot //g'
-
-printf "#LVM use: "
-if [ "$(sudo lvscan | grep -i ACTIVE | wc -l)" -gt 0 ] ; then printf "yes\n" ; else printf "no\n" ; fi
-
-printf "#Connections TCP : "
-ss -t | grep -i ESTAB | wc -l | tr -d '\n'
-printf " ESTABLISHED\n"
-
-printf "#User log: "
-who | wc -l
-
-printf "#Network: IP "
-/sbin/ifconfig | grep broadcast | sed 's/inet//g' | sed 's/netmask.*//g' | sed 's/ //g' | tr -d '\n'
-printf " ("
-/sbin/ifconfig | grep 'ether ' | sed 's/.*ether //g' | sed 's/ .*//g' | tr -d '\n'
-printf ")\n"
-
-printf "#Sudo : "
-grep 'sudo:' /var/log/auth.log | grep 'COMMAND=' | wc -l | tr -d '\n'
-printf " cmd\n"
